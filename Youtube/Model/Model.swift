a0efa9537e0a4b03aa778e0b48bbf630d9141a83
Menaim <ahmedmenaim8@gmail.com>
2020-10-05 21:30:13 +0400

    Second Version

---
+++ Youtube/Model/Model.swift
@@ -0,0 +1,9 @@
+//
+//  Model.swift
+//  Youtube
+//
+//  Created by Crypto on 10/5/20.
+//  Copyright © 2020 Crypto. All rights reserved.
+//
+
+import Foundation
---
+++ Youtube/Model/Video.swift
@@ -0,0 +1,9 @@
+//
+//  Video.swift
+//  Youtube
+//
+//  Created by Crypto on 10/5/20.
+//  Copyright © 2020 Crypto. All rights reserved.
+//
+
+import Foundation
