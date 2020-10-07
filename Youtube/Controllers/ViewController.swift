31fdbf36510ef49be19a213894d401a1c2a78407
Menaim <ahmedmenaim8@gmail.com>
2020-10-07 21:09:33 +0400

    Added TableView,Protocol & Display video titles

--- Youtube/Controllers/ViewController.swift
+++ Youtube/Controllers/ViewController.swift
@@ -8,15 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    let model = Model()
+class ViewController: UIViewController,UITableViewDataSource, UITableViewDelegate, ModelDelegate {
+    
+    @IBOutlet weak var videosTableView: UITableView!
+    
+    // MARK: - Vars
+    var model = Model()
+    var videos = [Video]()
+    
+    // MARK: - Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+        model.delegate = self
         model.getVideos()
     }
 
-
+    // MARK: - Model Delegate Methods
+    
+    func videosFetched(_ videos: [Video]) {
+        self.videos = videos
+        videosTableView.reloadData()
+        
+    }
+// MARK: - Table view methods
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return videos.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = videosTableView.dequeueReusableCell(withIdentifier: "VideoCell", for: indexPath)
+        let title = self.videos[indexPath.row].videoTitle
+        cell.textLabel?.text = title
+        return cell
+    }
+    
 }
 
